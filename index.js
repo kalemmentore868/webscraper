1ea1d98132ae1eddf07ef62ddd7965793a17c6e3
USER <user@USERs-MacBook-Air.local>
2023-08-03 16:45:25 -0400

    lit of hospitals obtained

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio')
+const fs = require('fs');
 
 const caribbeanCountries = [
   'Anguilla',
@@ -13,7 +14,7 @@ const caribbeanCountries = [
   'British Virgin Islands',
   'Cayman Islands',
   'Cuba',
-  'Curaçao',
+ 
   'Dominica',
   'Dominican Republic',
   'Grenada',
@@ -23,43 +24,79 @@ const caribbeanCountries = [
   'Martinique',
   'Montserrat',
   'Puerto Rico',
-  'Saint Barthélemy',
   'Saint Kitts and Nevis',
   'Saint Lucia',
   'Saint Martin',
   'Saint Vincent and the Grenadines',
-  'Sint Maarten',
+  
   'Trinidad and Tobago',
   'Turks and Caicos Islands',
   'United States Virgin Islands'
 ];
 
+const outliers = [ 'Saint Barthélemy', 'Curaçao','Sint Maarten']
+
+const allHopsitals = []
+
 async function run(){
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto("https://google.com");
+    const browser = await puppeteer.launch({headless: true});
+     const page = await browser.newPage();
+    for (let i = 0; i < caribbeanCountries.length; i++){
+ await page.goto("https://google.com");
 
     const searchInput = await page.$('input');
-    await searchInput.type("List of hospitals and medical centers in Trinidad and tobago")
+
+    await searchInput.type("List of hospitals and medical centers in " + caribbeanCountries[i])
 
     // Submit the search query
     await searchInput.press('Enter');
 
-    // Wait for the search results page to load (you can customize the wait time based on your internet speed)
-    await page.waitForTimeout(3000);
+
+     // Wait for the Shadow DOM element to appear
+   await page.waitForSelector('g-more-link a');
+
+    await page.click('g-more-link a');
+
+
+  // Wait for some time (optional)
+  await page.waitForTimeout(5000);
 
      const content = await page.content();
 
      const $ = cheerio.load(content);
-  const hospitalTitles = [];
-   $('div.rllt__details span.OSrXXb').each((index, element) => {
-    const title = $(element).text();
-    hospitalTitles.push(title);
-  });
 
-  console.log(hospitalTitles)
+     $('div.dbg0pd[role="heading"] span.OSrXXb').each((index, element) => {
+  allHopsitals.push($(element).text());
+});
+
+console.log(allHopsitals)
+console.log(caribbeanCountries[i])
+
+
+
+    }     
+  
+
+
 
     await browser.close();
+
+    const jsonData = JSON.stringify(allHopsitals, null, 2);
+    const filePath = './allhospitals.json';
+
+    fs.writeFile(filePath, jsonData, 'utf8', (err) => {
+  if (err) {
+    console.error('Error writing JSON file:', err);
+  } else {
+    console.log('JSON file has been saved successfully!');
+  }
+});
+
+
+
+
+
+
 }
 
 run()
